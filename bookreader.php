85453d429edf0f6ff21ec61394d810c4aa007e82
Phil Wilson <phil@philwilson.org>
2010-05-03 23:10:07 +0100

    First commit of some actual code, still mostly based on my podcast hosting code.
    Signed-off-by: Phil Wilson <phil@philwilson.org>

---
+++ bookreader.php
@@ -0,0 +1,48 @@
+<?php
+
+	require_once('ebookRead.php');
+	require_once('ebookData.php');
+
+	function metadata($ebook){
+		display("<b>Title:</b>", $ebook->getDcTitle());
+		display("<b>Creator:</b>", $ebook->getDcCreator());
+		display("<b>Description:</b>", $ebook->getDcDescription());
+		display("<b>ISBN or ID:</b>", $ebook->getDcIdentifier());
+		display("<b>Contributor(s):</b>", $ebook->getDcContributor());
+		display("<b>Contributor(s) Role:</b>", $ebook->getDcContributor("Role"));
+		display("<b>Language:</b>", $ebook->getDcLanguage());
+		display("<b>Rights:</b>", $ebook->getDcRights());
+		display("<b>Publisher</b>:", $ebook->getDcPublisher());
+		display("<b>Subject:</b>", $ebook->getDcSubject());
+		display("<b>Date:</b>", $ebook->getDcDate());
+		display("<b>Type:</b>", $ebook->getDcType());
+		display("<b>Format:</b>", $ebook->getDcFormat());
+		display("<b>Sources:</b>", $ebook->getDcSource());
+		display("<b>Relation:</b>", $ebook->getDcRelation());
+		display("<b>Coverage:</b>", $ebook->getDcCoverage());
+	}
+
+	function display($title, $data){
+	$info = "";
+	if(is_array($data)){
+		foreach($data as $element){
+			if($info == "")
+				$info = $element;
+			else
+				$info = $info.", ".$element;
+		}
+		$data = $info;
+	}
+	if($data != "")
+		echo $title." ".$data."\n <br />";
+
+	}
+
+	$ebookfile = "C:\\dropbox\\ebooks\\Beautiful_Code.epub";
+
+    //read our epub file
+	$ebook = new ebookRead($ebookfile);
+
+	metadata($ebook);
+	
+?>
---
+++ ebooks.php
@@ -0,0 +1,93 @@
+<?php
+
+include 'preg_find.php';
+
+// where are the files?
+$rootpath = "C:\dropbox\ebooks";
+
+$param = $_GET["dir"];
+
+$param = str_replace(".", "", $param);
+
+$dirname = $rootpath.$param."/";
+
+$dir = @opendir($dirname) or die("Unable to open $rootpath");
+
+//while ($p = readdir($dir)) {
+//    if (is_file($p) == FALSE && $p != "." && $p != "..") {
+
+        //$path = $rootpath . $p . "/";
+
+        // the request url without this filename
+        $scriptpath = explode("/", $_SERVER["REQUEST_URI"]);
+        // delete the last element
+        $scriptpath[count($scriptpath) - 1] = "";
+        $scriptpath = implode("/", $scriptpath);
+        $url = "http://" . $_SERVER["SERVER_NAME"].$scriptpath;
+        $scripturl = "http://" . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+
+        header('Content-type: application/xml');
+
+        $xml = new XmlWriter();
+        $xml->openMemory();
+        $xml->startDocument('1.0', 'UTF-8');
+
+        $xml->startElement('rss');
+        $xml->writeAttribute('version', '2.0');
+
+        $xml->startElement('channel');
+        $xml->writeElement('title', $param);
+        $xml->writeElement('link', $scripturl);
+
+        // sort the files by date so the most recent is first in the feed
+        $files = preg_find('/./', $dirname, PREG_FIND_RECURSIVE|PREG_FIND_RETURNASSOC |PREG_FIND_SORTMODIFIED|PREG_FIND_SORTDESC);
+        $files = array_keys($files);
+        
+        // add the files to the feed
+        foreach($files as $file) {
+
+            if (preg_match("/\.mp3$/", $file)) {
+                $ThisFileInfo = $getID3->analyze($file);
+
+                $xml->startElement('item');
+                $xml->writeElement('title', $ThisFileInfo['tags']['id3v2']['title'][0]);
+
+                $xml->writeElement('link', $url . $param . "/" . basename($file));
+
+                //Wed, 29 Apr 2009 16:32:52 GMT
+                $xml->writeElement('pubDate', date("D, d M Y H:i:s T", filemtime($file)));
+
+                $xml->startElement('guid');
+                $xml->writeAttribute('isPermaLink', 'false');
+                $xml->text($file);
+                $xml->endElement(); //</guid>
+
+
+
+                $xml->startElement('enclosure');
+                $xml->writeAttribute('url', $url . $param . "/" . basename($file));
+                $xml->writeAttribute('length', filesize($file));
+                $xml->writeAttribute('type', 'audio/mpeg');
+                $xml->endElement(); //</enclosure>
+
+                $xml->endElement(); //</item>
+            }
+        }
+        //closedir($dir_handle);
+
+        $xml->endElement(); // </channel>
+        $xml->endElement(); // </rss>
+        echo $xml->outputMemory(true);
+
+        //$f = $path."/".$p.".rss";
+        //$fh = fopen($f, 'w') or die ("can't open file ".$f);
+        //fwrite($fh, $xml->outputMemory(true));
+        //fclose($fh);
+    //}
+
+//}
+
+closedir($dir);
+#echo "Done!";
+
+?>
